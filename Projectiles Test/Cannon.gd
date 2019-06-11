d183ddb12d8182747ddc844f6fe62ac4b659e0d6
MiloTranseunte <50523240+MiloTranseunte@users.noreply.github.com>
2019-06-11 19:31:35 -0300

    Prototype for a Spider done
    It's a spider that can shoot from ceiling or from the floor, within an angle. Can move left or right, detect walls and if it is about to fall to change its direction.

--- Projectiles Test/Cannon.gd
+++ Projectiles Test/Cannon.gd
@@ -11,44 +11,45 @@ export (float) var velocity = 20
 export (Vector2) var motion = Vector2()
 
 var facing = 1
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	motion.y += gravity * delta
+	motion.y -= gravity * delta # or motion.y += gravity * delta
 	
+	if is_on_ceiling():
+		print("on_ceiling")
+		if !$ray_left.is_colliding() or !$ray_right.is_colliding():
+			print("some ray is not colliding")
+			facing *= -1
+
+	motion.x = velocity * facing
+
 	var knife_pos = 0 # $pos_left_cannon.global_position
 	var knife = KNIFE
 	
-	motion.x = velocity * facing
-	
-	if !$ray_right.is_colliding():
-		print("right not collide")
-		facing = -1
-	if !$ray_left.is_colliding():
-		print("left not collide")
-		facing = 1
-	
-	if is_on_wall():
-		print("wall")
-		motion.x = velocity * -1
-	
 	if Input.is_action_just_pressed("throw"): # SPACEBAR
+		var _shoot_direction = -1 # negative left
 		knife_pos = $pos_left_cannon.global_position
 		knife = KNIFE.instance()
 		get_parent().add_child(knife)
 		knife.position = knife_pos
-		knife.launch(-1)
+		knife.launch(_shoot_direction)
 		
+		knife.beforeVanish()
 		
+		_shoot_direction *= -1 # EQUALS TO +1 positive right
 		knife_pos = $pos_right_cannon.global_position
 		knife = KNIFE.instance()
 		get_parent().add_child(knife)
 		knife.position = knife_pos
-		knife.launch(1)
+		knife.launch(_shoot_direction)
 		
 		knife.beforeVanish()
 		
 	motion = move_and_slide(motion, Vector2(0, -1))
+		
+	if is_on_wall():
+		print("wall")
+		facing *= -1
--- Projectiles Test/knife.gd
+++ Projectiles Test/knife.gd
@@ -7,22 +7,14 @@ var force = 1
 var gravity = 300
 var velocity = Vector2.ZERO
 
-# to take on acount: get_viewport().get_mouse_position()
-
-var absoluteValue = Vector2.ZERO
+# to take into account: get_viewport().get_mouse_position()
 
 func beforeVanish():
+	$Timer.set_wait_time(5)
 	$Timer.start()
 
 func _physics_process(delta):
-	var angles = Vector2(1, -11)#.angle()
-	#print(rad2deg(angles))
-	absoluteValue = angles.abs()
-	#print(rad2deg(absoluteValue.angle()))
-	
-	
 	velocity.y += gravity * delta
-	#velocity.x = clamp(velocity.x, 10, 1000)
 	
 	var collision = move_and_collide(velocity * delta)
 	
@@ -33,13 +25,14 @@ func _physics_process(delta):
 
 
 func launch(direction):
-	velocity = THROW_VELOCITY * Vector2(direction, 1) # Vector2(direction, 1) es para dar vuelta el Vector, es como decir velocity = velocity * direction (-1 or 1)
-	
+	velocity = THROW_VELOCITY * Vector2(direction, -1) # segundo término, cambia entre (1) y (-1)
+
+
 func _on_impact(normal : Vector2):
 	velocity = velocity.bounce(normal)
 	velocity *= 0.5 + rand_range(-0.05, 0.05)
 	pass
 
+
 func _on_Timer_timeout():
-	print("Knives out!")
 	queue_free()
