534db22eb5498a9783cd82cffb700d70ca4c9103
MiloTranseunte <50523240+MiloTranseunte@users.noreply.github.com>
2019-06-11 13:27:19 -0300

    Ray system a throw of knives

--- Projectiles Test/Cannon.gd
+++ Projectiles Test/Cannon.gd
@@ -6,6 +6,10 @@ const KNIFE = preload("res://knife.tscn")
 export (float) var angle = 45
 export (float) var shoot_force = 50
 
+export (float) var gravity = 20
+export (float) var velocity = 20
+export (Vector2) var motion = Vector2()
+
 var facing = 1
 
 # Called when the node enters the scene tree for the first time.
@@ -13,25 +17,38 @@ func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	var cannon_bal_pos = $Position2D.global_position
-	var cannon_ball = CANNON_BALL
+	motion.y += gravity * delta
 	
-	var knife_pos = $Position2D.global_position
+	var knife_pos = 0 # $pos_left_cannon.global_position
 	var knife = KNIFE
 	
-	if Input.is_action_just_pressed("shoot"): # SPACEBAR
-		print("shoot")
-		cannon_ball = CANNON_BALL.instance()
-		get_parent().add_child(cannon_ball)
-		cannon_ball.position = cannon_bal_pos
-		
-		cannon_ball.set_angle(angle)
-		cannon_ball.set_shoot_force(shoot_force)
-		cannon_ball.set_launch_position(self.global_position)
-		
+	motion.x = velocity * facing
+	
+	if !$ray_right.is_colliding():
+		print("right not collide")
+		facing = -1
+	if !$ray_left.is_colliding():
+		print("left not collide")
+		facing = 1
+	
+	if is_on_wall():
+		print("wall")
+		motion.x = velocity * -1
+	
 	if Input.is_action_just_pressed("throw"): # SPACEBAR
-		print("throw")
+		knife_pos = $pos_left_cannon.global_position
 		knife = KNIFE.instance()
 		get_parent().add_child(knife)
 		knife.position = knife_pos
-		knife.launch(facing)
+		knife.launch(-1)
+		
+		
+		knife_pos = $pos_right_cannon.global_position
+		knife = KNIFE.instance()
+		get_parent().add_child(knife)
+		knife.position = knife_pos
+		knife.launch(1)
+		
+		knife.beforeVanish()
+		
+	motion = move_and_slide(motion, Vector2(0, -1))
--- Projectiles Test/knife.gd
+++ Projectiles Test/knife.gd
@@ -1,12 +1,26 @@
 extends KinematicBody2D
 
 const THROW_VELOCITY = Vector2(300, -200)
-export (float) var angle = rad2deg(45)
+#						Vector angle.y * magnitud, angle.y * magnitud
+export (float) var angle = deg2rad(2574)
 var force = 1
 var gravity = 300
 var velocity = Vector2.ZERO
 
+# to take on acount: get_viewport().get_mouse_position()
+
+var absoluteValue = Vector2.ZERO
+
+func beforeVanish():
+	$Timer.start()
+
 func _physics_process(delta):
+	var angles = Vector2(1, -11)#.angle()
+	#print(rad2deg(angles))
+	absoluteValue = angles.abs()
+	#print(rad2deg(absoluteValue.angle()))
+	
+	
 	velocity.y += gravity * delta
 	#velocity.x = clamp(velocity.x, 10, 1000)
 	
@@ -15,7 +29,9 @@ func _physics_process(delta):
 	if collision != null:
 		_on_impact(collision.normal)
 	
-		
+	pass
+
+
 func launch(direction):
 	velocity = THROW_VELOCITY * Vector2(direction, 1) # Vector2(direction, 1) es para dar vuelta el Vector, es como decir velocity = velocity * direction (-1 or 1)
 	
@@ -23,3 +39,7 @@ func _on_impact(normal : Vector2):
 	velocity = velocity.bounce(normal)
 	velocity *= 0.5 + rand_range(-0.05, 0.05)
 	pass
+
+func _on_Timer_timeout():
+	print("Knives out!")
+	queue_free()
